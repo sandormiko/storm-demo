1700701447723e8b683c1b99eb4959f47ee7202d
Sándor Mikó <miko@elte.hu>
2015-12-28 17:04:13 +0100

    Removed cache

--- src/main/java/hu/sm/storm/topology/bolt/MessageVerificationBolt.java
+++ src/main/java/hu/sm/storm/topology/bolt/MessageVerificationBolt.java
@@ -8,8 +8,6 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.Injector;
-
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
@@ -17,8 +15,6 @@ import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
 import hu.sm.storm.base.AppConstants;
 import hu.sm.storm.base.domain.KafkaMessage;
-import hu.sm.storm.service.api.MessageVerifierService;
-import hu.sm.storm.service.injector.InjectorProvider;
 import hu.sm.storm.topology.hook.BoltMetricsHook;
 
 @SuppressWarnings("serial")
@@ -26,20 +22,14 @@ public class MessageVerificationBolt extends BaseRichBolt {
 
 	private static final String C_NAME = MessageVerificationBolt.class.getName();
 	private static Logger LOG = LoggerFactory.getLogger(MessageVerificationBolt.class);
-	private static Injector injector = null;
 
 	private Map<String, Set<KafkaMessage>> rootTopicMessagesByTopicId = null;
-	private Map<String, Set<KafkaMessage>> randomTopicMessagesByTopicId = null;
 	private OutputCollector collector = null;
-	private MessageVerifierService verifierSrv;
 
 	@Override
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector aCollector) {
 		this.collector = aCollector;
-		randomTopicMessagesByTopicId = new HashMap<>();
 		rootTopicMessagesByTopicId = new HashMap<>();
-		injector = InjectorProvider.getInjector();
-		verifierSrv = injector.getInstance(MessageVerifierService.class);
 		context.addTaskHook(new BoltMetricsHook());
 	}
 
@@ -53,27 +43,24 @@ public class MessageVerificationBolt extends BaseRichBolt {
 			String topic = input.getStringByField(AppConstants.TOPIC);
 			KafkaMessage msg = (KafkaMessage) input.getValueByField(AppConstants.KAFKA_MESSAGE);
 			if (input.getSourceComponent().equals(AppConstants.KAFKA_MSG_EXTRACTOR)) {
-				Set<KafkaMessage> messagesOnTopic = getMessagesByTopic(rootTopicMessagesByTopicId, topic);
+				Set<KafkaMessage> messagesOnTopic = getMessagesByTopic(topic);
 				messagesOnTopic.add(msg);
 				rootTopicMessagesByTopicId.put(topic, messagesOnTopic);
 			} else {
-				Set<KafkaMessage> messagesOnTopic = getMessagesByTopic(randomTopicMessagesByTopicId, topic);
-				messagesOnTopic.add(msg);
-				randomTopicMessagesByTopicId.put(topic, messagesOnTopic);
+				Set<KafkaMessage> messagesOnTopic = getMessagesByTopic(topic);
+				boolean removed = messagesOnTopic.remove(msg);
+				if (!removed) {
+					String infoMsg = String.format("Message %s has not been received on topic %s", msg.toString(), topic);
+					LOG.warn(infoMsg);
+				}
+				
+				if (messagesOnTopic.size() == 0) {
+					if (LOG.isInfoEnabled()) {
+						LOG.info("All messages have been received on topic " + topic);
+					}
+				}
 			}
-			boolean nrOfMessagesMatchesOnAllTopic = verifierSrv.nrOfMessagesMatchsOnAllTopic(rootTopicMessagesByTopicId,
-					randomTopicMessagesByTopicId);
-			boolean allMessagesHasBeenVerified = false;
-			if (nrOfMessagesMatchesOnAllTopic) {
-				allMessagesHasBeenVerified = verifierSrv.verifyMessages(rootTopicMessagesByTopicId,
-						randomTopicMessagesByTopicId);
-			}
-			if (LOG.isInfoEnabled()) {
-				LOG.info("All messages have been verified " + allMessagesHasBeenVerified);
-			}
-			if(allMessagesHasBeenVerified){
-				clearCache();
-			}
+
 			collector.ack(input);
 		} finally {
 			if (LOG.isDebugEnabled()) {
@@ -87,16 +74,12 @@ public class MessageVerificationBolt extends BaseRichBolt {
 		// No output fields to declare
 	}
 
-	private Set<KafkaMessage> getMessagesByTopic(Map<String, Set<KafkaMessage>> messagesbyTopicMap, String topic) {
-		Set<KafkaMessage> messagesOnTopic = messagesbyTopicMap.get(topic);
+	private Set<KafkaMessage> getMessagesByTopic(String topic) {
+		Set<KafkaMessage> messagesOnTopic = rootTopicMessagesByTopicId.get(topic);
 		if (messagesOnTopic == null) {
 			messagesOnTopic = new HashSet<>();
 		}
 		return messagesOnTopic;
 	}
-	
-	private void clearCache(){
-		randomTopicMessagesByTopicId.clear();
-		rootTopicMessagesByTopicId.clear();
-	}
+
 }
